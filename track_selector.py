9372f71501c9f599522f0f67f9d839149bb9286d
KalebSchmidlkofer <kaleb@ashbyte.com>
2023-10-11 22:59:30 -0700

    update: added video selects to options

--- track_selector.py
+++ track_selector.py
@@ -12,31 +12,62 @@ def data(path, file):
   try:
     # print(f'File: {file}')
     probe = ffmpeg.probe(os.path.join(path, file))
+    video_tracks = [stream for stream in probe['streams'] if stream['codec_type'] == 'video']
     audio_tracks = [stream for stream in probe['streams'] if stream['codec_type'] == 'audio']
     subtitle_tracks = [stream for stream in probe['streams'] if stream['codec_type'] == 'subtitle']
-    subs_streams = []
-    subtitles = {}
-    streams = []
-    tracks = {}
+    video_stream = []
+    video_track = {}
+    sub_stream = []
+    sub_track = {}
+    audio_stream = []
+    audio_track = {}
     count = 0
 
-    for index, audio_track in enumerate(audio_tracks):
-      streams.append(audio_track.get('tags', {}).get('language', 'N/A'))
+    for index, video_tracks in enumerate(video_tracks):
+      video_stream.append(video_track.get('tags', {}).get('language', 'N/A'))
 
-    for index, subtitle_track in enumerate(subtitle_tracks):
-      subs_streams.append(subtitle_track.get('tags', {}).get('language', 'N/A'))
-    
-    for x in subs_streams:
-      subtitles[count][x] = f'0:s:{count}'
+    for index, audio_tracks in enumerate(audio_tracks):
+      audio_stream.append(audio_tracks.get('tags', {}).get('language', 'N/A'))
+
+    for index, subtitle_tracks in enumerate(subtitle_tracks):
+      sub_stream.append(subtitle_tracks.get('tags', {}).get('language', 'N/A'))
+
+
+    for x in video_stream:
+      if x not in video_track:
+        video_track[x] = []
+      video_track[x].append(f'0:s:{count}') 
       count+=1
-    print(subtitles)
+    print(f'video: {video_track}')
+
     count=0
-    for x in streams:
-      tracks[x] = f'0:a:{count}'
+    for x in audio_stream:
+      if x not in audio_track:
+        audio_track[x] = []
+      audio_track[x].append(f'0:a:{count}')  
       count += 1
+    print(f'audio: {audio_track}')
+    
+    count=0
+    for x in sub_stream:
+      if x not in sub_track:
+        sub_track[x] = []
+      sub_track[x].append(f'0:s:{count}') 
+      count+=1
+    print(f'subs: {sub_track}')
+    
+
+    selected_video = inquirer.checkbox(
+      message='Select all Video tracks you want to keep',
+      choices=video_stream,
+      instruction="([\u2191\u2193]: Select Item. [Space]: Toggle Choice), [Enter]: Confirm",
+      validate=lambda result: len(result) >= 1,
+      invalid_message="should be at least 1 selection",
+      ).execute()
+    
     selected_streams = inquirer.checkbox(
       message='Select all Audio tracks you want to keep',
-      choices=tracks,
+      choices=audio_stream,
       instruction="([\u2191\u2193]: Select Item. [Space]: Toggle Choice), [Enter]: Confirm",
       validate=lambda result: len(result) >= 1,
       invalid_message="should be at least 1 selection",
@@ -44,11 +75,12 @@ def data(path, file):
     
     selected_subtitles = inquirer.checkbox(
       message='Select all Subtitle tracks you want to keep',
-      choices=subs_streams,
+      choices=sub_stream,
       instruction="([\u2191\u2193]: Select Item. [Space]: Toggle Choice), [Enter]: Confirm",
       validate=lambda result: len(result) >= 1,
       invalid_message="should be at least 1 selection",
       ).execute()
+
 
     for x in selected_subtitles:
       del subtitles[x]
@@ -56,7 +88,7 @@ def data(path, file):
     for x in selected_streams:
       del tracks[x]
 
-    return tracks, subtitles
+    return video_track, audio_track, sub_track
   except ffmpeg.Error as e:
     # print(f'stdrr: {e.stderr}\n')
     # print(f'stdout:{e.stdout}')
@@ -81,21 +113,21 @@ def main():
   count = 1
   
   for x in enumerate(src_list):
-    tracks, subtitles = data(src_path , x[1])
+    video, tracks, subtitles = data(src_path , x[1])
 
-    # print(tracks)
-    # print(subtitles)
-    # audio_map=''.join([f'-map {tracks[stream]} ' for stream in tracks])
-    # filename = f'{str(x[1])}'
-    # filename = filename[:-4]
-    # output_file = os.path.join(f'{src_path}', f'{filename}-eng.mkv')
-    # ep_path = os.path.join(src_path, x[1])
-# 
-    # command = f'''ffmpeg -i "{ep_path}" -map 0 {audio_map}-map 0:s:0 -c copy "{output_file}"'''
-    # subprocess.run(command)
-    # print(command)
-# 
-    # count += 1
+    print(tracks)
+    print(subtitles)
+    audio_map=''.join([f'-map {tracks[stream]} ' for stream in tracks])
+    filename = f'{str(x[1])}'
+    filename = filename[:-4]
+    output_file = os.path.join(f'{src_path}', f'{filename}-eng.mkv')
+    ep_path = os.path.join(src_path, x[1])
+
+    command = f'''ffmpeg -i "{ep_path}" -map 0 {audio_map}-map 0:s:0 -c copy "{output_file}"'''
+    subprocess.run(command)
+    print(command)
+
+    count += 1
 
     # input_file = ffmpeg.input(ep_path)
     # mapped = input_file.output(f'{output_file}', map='v:0,a:1', c='copy')
