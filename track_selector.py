52faab0278be3d8acf2ea517e9c8b476f671977a
KalebSchmidlkofer <kaleb@ashbyte.com>
2023-10-19 23:15:53 -0700

    Everything except concat working

--- track_selector.py
+++ track_selector.py
@@ -9,68 +9,79 @@ import time
 app = typer.Typer()
 
 def data(path, file):
-  # print(f'File: {file}')
   start = time.time()
   probe = ffmpeg.probe(os.path.join(path, file))
   audio_tracks = [stream for stream in probe['streams'] if stream['codec_type'] == 'audio']
   subtitle_tracks = [stream for stream in probe['streams'] if stream['codec_type'] == 'subtitle']
   sub_stream = []
   sub_track = {}
+  sub_list = []
   audio_stream = []
+  audio_list = []
   audio_track = {}
+  returned_list = []
   count = 0
 
   for index, audio_tracks in enumerate(audio_tracks):
-    audio_stream.append(audio_tracks.get('tags', {}).get('language', 'N/A'))
-
+    audio_stream.append(f"{audio_tracks.get('tags', {}).get('language', 'N/A')}{count}")
+    count +=1
+     
+  count = 0
   for index, subtitle_tracks in enumerate(subtitle_tracks):
-    sub_stream.append(subtitle_tracks.get('tags', {}).get('language', 'N/A'))
+    sub_stream.append(f"{subtitle_tracks.get('tags', {}).get('language', 'N/A')}{count}")
+    count +=1
 
   count=0
   for x in audio_stream:
-    if x not in audio_track:
-      audio_track[x] = []
-    audio_track[x].append(f'0:a:{count}')  
+    if x in audio_track:
+      x = f'{x}{count}'
+    audio_track[x] = f'0:a:{count}'
     count += 1
   
   count=0
   for x in sub_stream:
-    if x not in sub_track:
-      sub_track[x] = []
-    sub_track[x].append(f'0:s:{count}') 
+    if x in sub_stream:
+      x = f'{x}'
+    sub_track[x] = f'0:s:{count}'
     count+=1
+  
+  count=0
+  for x in audio_stream:
+    audio_list.append(x)
+    count += 1
+
+  count=0
+  for x in sub_stream:
+    sub_list.append(x)
+    count += 1
+
   end = time.time()
   print(f'Execute time: {end-start}')
 
   selected_audio = inquirer.checkbox(
     message='Select all Audio tracks you want to keep',
-    choices=audio_track,
+    choices=audio_list,
     instruction="([\u2191\u2193]: Select Item. [Space]: Toggle Choice), [Enter]: Confirm",
     validate=lambda result: len(result) >= 1,
     invalid_message="should be at least 1 selection",
     ).execute()
-  print(selected_audio)
+  
   selected_subtitles = inquirer.checkbox(
     message='Select all Subtitle tracks you want to keep',
-    choices=sub_stream,
+    choices=sub_list,
     instruction="([\u2191\u2193]: Select Item. [Space]: Toggle Choice), [Enter]: Confirm",
     validate=lambda result: len(result) >= 1,
     invalid_message="should be at least 1 selection",
     ).execute()
 
   for x in selected_audio:
-    print(f'audio track: {audio_track}')
-    print(f'selected audio: {x}')
-    # for y in audio_track[]
-    # del audio_track[x]
+    returned_list.append(audio_track[x])
+      
 
   for x in selected_subtitles:
-    print(f'subtitle track: {sub_track}')
-    print(f'selected subtitles: {x}')
-    # del sub_track[x]
+    returned_list.append(sub_track[x])
     
-      
-  return audio_track, sub_track
+  return returned_list
   
 
 def main():
@@ -83,25 +94,30 @@ def main():
   
   src_list = os.listdir(src_path)
   # print(src_path)
-  count = 1
   
   for x in enumerate(src_list):
-    tracks, subtitles = data(src_path , x[1])
-
-    # print(videos)
-    # print(tracks)
-    # print(subtitles)
+    files = []
+    tracks = data(src_path , x[1])
+    tracks.append('0:v:0')
     
     filename = f'{str(x[1])}'
     filename = filename[:-4]
-    output_file = lambda n: os.path.join(f'{src_path}', f'{n}.mkv')
+    output_file = lambda n, c: os.path.join(f'{src_path}', f'{n}{c}.mkv')
+    output_file2 = lambda n: os.path.join(f'{src_path}', f'{n}.mkv')
     ep_path = os.path.join(src_path, x[1])
 
     input_file = ffmpeg.input(ep_path)
-    mapped = input_file.output(f'{output_file("a")}', map='0:a:0', c='copy')
-    ffmpeg.run(mapped)
-
-    count += 1
+    count = 0
+    for x in tracks:
+      mapped = input_file.output(f'{output_file(str(x)[2:-2], count)}', map=x, c='copy')
+      ffmpeg.run(mapped)
+      files.append(f'{str(x)[2:-2]}{count}.mkv')
+      count += 1
+    
+    audio_map=''.join([f'{output_file2({f"{os.path.join(src_path, x)}"})}, ' for x in files])
+    concat = ffmpeg.concat(audio_map)
+    cmd = ffmpeg.output(concat, f'{filename}.mkv')
+    ffmpeg.run(cmd)
 
 if __name__ == "__main__":
   # asyncio.run(main())
